1ab88841a122c95c31ffa261d63be037494dda17
kyawphyowai94.github.io <kyawphyowai.sgg@gmail.com>
2024-11-28 23:44:09 +0630

    KyawOnlineShop

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function shop(){
+        return view('front.shops');
+    }
+
+    public function shopItem($id){
+        return view('front.shop-item');
+    }
+}
